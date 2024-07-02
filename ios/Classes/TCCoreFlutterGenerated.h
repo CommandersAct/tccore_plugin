6cf14399f41087d13962cd8a0a87588969148011
Abdelhakim SAID <abdelhakim.said@commandersact.com>
2024-07-02 13:21:32 +0200

    1.2.2

---
+++ ios/Classes/TCCoreFlutterGenerated.h
@@ -0,0 +1,16 @@
+//
+//  TCCoreFlutterGenerated.h
+//  tccore_plugin
+//
+//  Created by Abdelhakim SAID on 02/07/2024.
+//
+
+#ifndef TCCoreFlutterGenerated_h
+#define TCCoreFlutterGenerated_h
+
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const TCCoreFlutterVersion;
+
+#endif /* TCCoreFlutterGenerated_h */
